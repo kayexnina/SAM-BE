228d9fbe35c6b4ca50409830a8dfbf30170160ab
kayexnina <kncristobal@iskolarngbayan.pup.edu.ph>
2025-01-08 01:13:42 +0800

    I’m still working on issue #2

--- F01/script.js
+++ F01/script.js
@@ -55,9 +55,10 @@ window.addEventListener('scroll', function () {
 
 window.onscroll = function () {
   const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) {  
+  if (window.scrollY > 20) {  
       navbar.classList.add('scrolled');
   } else {
       navbar.classList.remove('scrolled');
   }
 };
+
